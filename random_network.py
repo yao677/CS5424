c8cfabb3c9469074d0940bf95e9fb4ed2c053fab
yao677 <jacky92@vt.edu>
2018-04-18 16:54:37 -0400

    first commit

---
+++ random_network.py
@@ -0,0 +1,8 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+#generate a random graph
+#Our sample network consists of 100,000 nodes, and average connectivity of node
+#ranges from 5 to 25
+G=fast_gnp_random_graph()
